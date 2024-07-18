659edaa9748f447e08dd9f05c0e64c952b43d9ac
hrSajib <harun15-4293@diu.edu.bd>
2024-07-18 13:58:09 +0600

    user approval done

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,40 +1,29 @@
 import axios, { all } from 'axios';
+import e from 'cors';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const AdminDashboard = () => {
     const navigate = useNavigate();
 
     const [allUsers, setAllUsers] = useState([]);
 
+    const fetchUsers =()=>{
+            axios.get('http://localhost:5500/getUsers')
+                .then(r => {
+                    const all = r.data;
+                    const withOutAdminAll =all.filter(u=> u.role !== 'admin');
+                    r.data && setAllUsers(withOutAdminAll);
+                })
+                .catch(err => {
+                    console.error('Error fetching users:', err);
+                });
+    }
+
     useEffect(() => {
-        axios.get('http://localhost:5500/getUsers')
-            .then(r => {
-                const all = r.data;
-                const withOutAdminAll =all.filter(u=> u.role !== 'admin');
-                r.data && setAllUsers(withOutAdminAll);
-            })
-            .catch(err => {
-                console.error('Error fetching users:', err);
-            });
+        fetchUsers();
     }, []);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -54,7 +43,7 @@ const AdminDashboard = () => {
                 <h1 className="text-2xl font-bold text-center">Admin Dashboard</h1>
             </header>
             <div className='w-full flex justify-end pr-5'>
-                <button onClick={handleLogOut} className='bg-red-200 p-2 mt-5 rounded-lg font-bold'>Log Out</button>
+                <button onClick={handleLogOut} className='bg-red-200 p-2 mt-5 rounded-md font-bold'>Log Out</button>
             </div>
 
             <main className="flex flex-col items-center mt-10 w-full px-6">
@@ -81,7 +70,7 @@ const AdminDashboard = () => {
                             <p className='border w-[20%] text-left'>Name</p> <p className='border w-[20%]'>Role Requested</p><p className='border w-[20%]'>Status</p> <p className='border w-[30%] text-right'>Approval</p>
                         </div>
                         {
-                            allUsers.map(user => <Row user={user} key={user._id}></Row>)
+                            allUsers.map(user => <Row user={user} key={user._id} fetchUsers={fetchUsers}></Row>)
                         }
                     </div>
                 </section>
@@ -125,19 +114,47 @@ export default AdminDashboard;
 
 
 
-const Row =({user})=>{
+const Row =({user , fetchUsers})=>{
+
+    const handleUpdateStatus = (e)=>{
+
+        e.preventDefault();
+        const value = (e.target.approval.value)
+        
+        axios.put(`http://localhost:5500/userApproval/${user._id}`, {value}, {
+            headers: {
+                "Content-Type":'application/json'
+            }
+        })
+        .then(r=>{
+            r.data && Swal.fire({
+                title: 'Status Updated',
+                icon: 'success',
+                confirmButtonText: 'OK',
+                customClass: {
+                    confirmButton: 'custom-confirm-button'
+                }
+            })
+            fetchUsers();
+        })
+
+    
+    }
+
+
+        
     
     return(
         <div className='flex justify-between text-left bg-gray-100 p-1 mb-1 px-2'>
             <p className=' w-[20%]'>{user.name}</p>
             <p className=' w-[20%] text-center'>{user.role}</p>
-            <p className=' w-[20%] text-center'>{user.status}</p>
+            <p className={user.status == 'active' ? `text-center w-[20%] text-center1 text-green-700` : user.status == 'blocked' ?  `text-center w-[20%] text-center1 text-red-700` :  `text-center w-[20%] text-center1`  }>{user.status}</p>
             <div className=' w-[30%] flex justify-end'>
-                <form>
+                <form onSubmit={handleUpdateStatus}>
                     <select className='bg-gray-600 text-white p-[6px] mr-2 ' name="approval">
-                        <option value="active">Choose</option>
+                        <option value="pending">Pending</option>
                         <option value="active">Approve</option>
-                        <option value="active">Decline</option>
+                        <option value="blocked">Block</option>
                     </select>
                     <input className='bg-gray-600 p-1 text-white  px-3' type="submit" value="Apply" />
 
